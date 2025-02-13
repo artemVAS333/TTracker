ce60fe2b63b8a6bdeb48a661918c102f6e10810f
artemVAS333 <artem.vas333@gmail.com>
2025-02-13 09:34:29 +0200

    edit main.ts electron

--- apps/desktop/electron/main.ts
+++ apps/desktop/electron/main.ts
@@ -73,9 +73,7 @@ app.on('window-all-closed', () => {
 app.on('activate', () => {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow()
-  }
+  if (BrowserWindow.getAllWindows().length === 0) createWindow()
 })
 
 app.whenReady().then(createWindow)
