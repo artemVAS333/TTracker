a2f02c37c3b6b2562b02636cbc9fb920ac3985f4
artemVAS333 <artem.vas333@gmail.com>
2025-02-13 09:09:50 +0200

    add desktop

---
+++ apps/desktop/src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+  return (    
+  <button
+    onClick={() => {
+      window.electron.store.set('foo', 'bar');
+      // or
+      console.log(window.electron.store.get('foo'));
+    }}
+  >
+    Click Me!
+</button>
+  )
+}
+
+export default App
