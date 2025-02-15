bda26462b84df4bcc056cfaf7089e2f4980e944d
artemVAS333 <artem.vas333@gmail.com>
2025-02-15 19:38:26 +0200

    just commit

--- apps/desktop/src/main.tsx
+++ apps/desktop/src/main.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
+import { Provider } from 'react-redux';
+
 import './assets/scss/index.scss';
 import './assets/styles/index.css';
+import store from './redux/store.ts';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
 );
 
